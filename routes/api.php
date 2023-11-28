81a0b3e408a6c8a85edc1876d98b9d9be0349c2d
celeEvora <celeste13kick@gmail.com>
2023-11-28 05:01:41 -0600

    update api.php

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,9 @@
 use App\Http\Controllers\GenreController;
 use App\Http\Controllers\BookController;
 use App\Http\Controllers\UserController; 
+use App\Http\Controllers\OrderController; 
+use App\Http\Controllers\CartController; 
+
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController; 
@@ -30,31 +33,50 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 |
 */
 
-Route::post('auth/register', [AuthController::class, 'create']);
-Route::post('auth/login', [AuthController::class, 'login']);
 
-// Route::middleware(['auth:sanctum'])->group(function () {
-//     Route::get('auth/logout', [AuthController::class, 'logout']);
-// });
+Route::controller(AuthController::class)->group(function(){
+    Route::post('/register', 'register');
+    Route::post('/login', 'login');
+    Route::get('/logout', 'logout');
+});
 
-// Route::controller(BookController::class)->group(function(){
-//     Route::get('/catalog', 'showCatalog');
-//     Route::get('/catalog-genre', 'ShowBookByGenre');
-//     Route::post('/catalog/store-book', 'store');
-//     Route::get('/catalog/{id}', 'find');
-//     Route::put('/catalog/{id}', 'update');
-//     Route::delete('/catalog/{id}', 'destroy');
+//private routes
+Route::middleware(['auth:sanctum','is.admin'])->group(function() {
 
-// });
+    Route::controller(BookController::class)->group(function(){
+        Route::post('/catalog/store-book', 'store');
+        Route::put('/catalog/{id}', 'update');
+        Route::delete('/catalog/{id}', 'destroy');
 
-// Route::get('/genres',[GenreController::class, 'showGenres']);
+    });
 
-// Route::controller(UserController::class)->group(function(){
-//     Route::get('/users',[UserController::class, 'showUsers']);
-//     Route::delete('/users/{id}',[UserController::class, 'destroy']);
-// });
+    Route::controller(UserController::class)->group(function(){
+        Route::get('/users', 'showUsers');
+        Route::delete('/users/{id}', 'destroy');
+    });
 
-// Route::controller(BookController::class)->group(function(){
-//     Route::get('/catalog', 'showCatalog']);
-//     Route::get('/catalog', 'showCatalog']);
-// });
+    Route::controller(OrderController::class)->group(function(){
+        Route::get('/orders', 'showOrders');
+        Route::put('/orders/{orderId}', 'cancelOrder');
+    });
+
+    Route::post('/genres',[GenreController::class, 'store']);
+
+});
+
+// public routes
+Route::controller(BookController::class)->group(function(){
+    Route::get('/catalog', 'showCatalog');
+    Route::get('/catalog-genre', 'ShowBookByGenre');
+    Route::get('/catalog/{id}', 'find');
+});
+
+Route::get('/genres',[GenreController::class, 'showGenres']);
+
+Route::controller(CartController::class)->group(function(){
+    Route::post('/cart-add/{id}', 'addToCart');
+    Route::post('/cart-put', 'updateCart');
+    Route::delete('/cart-remove/{id}', 'removeItem');
+    Route::get('/cart', 'getCart');
+});
+
